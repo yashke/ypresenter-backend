8ca063bfc727c3df1c6543fa98fc097dc5253526
Jan Filipowski <jachuf@gmail.com>
2013-01-20 18:35:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YpresenterBackend::Application.config.secret_token = '46ec52d729decc68344342d8912534e1e418ff79d4dc59588affd99c9865bd2c29bd88822d7430532109c5a61710bfad1dc12eb4de98458fdbe5e5bb311f2886'
